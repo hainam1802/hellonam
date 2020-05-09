e94e207eea45cb5fe225691376dae47e15926fc5
iahman8101 <nhuc69@gmail.com>
2020-05-09 21:11:36 +0700

    eee

---
+++ 333333.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	int a;
+	printf("Nhap vao 1 so \n");
+	scanf("%d",&a);
+	if(a==2){printf("thu 2");
+	}
+	else if(a==3){printf("thu 3");
+	}
+	else if(a==4){printf("thu 4");
+	}
+	else if(a==5){printf("thu 5");
+	}
+	else if(a==6){printf("thu 6");
+	}
+	else if(a==7){printf("thu 7");
+	}
+	else if(a==8){printf("chu nhat");
+	}
+	else {printf("khong phai ngay trong tuan");
+	}}
