785ede523aa204a77e1a25d0abe8c4a2a1e15f59
iahman8101 <nhuc69@gmail.com>
2020-06-02 20:02:05 +0700

    aa

--- bai2.cpp
+++ bai2.cpp
@@ -1,21 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+
 int main(){
-	int a,b,c;
-	printf ("Nhap 3 so \n");
-	printf("so thu nhat :\n");
-	scanf("%d",&a);
-	printf("so thu hai :\n");
-	scanf("%d",&b);
-	printf("so thu ba :\n");
-	scanf("%d",&c);
-	if (a<b){
-		if(a<c){printf("min %d",a);}
-		else {printf("min %d",c);}
+	char s[50];
+	int count1, count2, count3, count4, count5;
+	count1 = count2 = count3 = count4 = count5 = 0;
+	printf("Enter string: ");
+	scanf("%s",s);
+	for(int i=0; i<strlen(s); i++){
+		if(s[i] == 'a')
+			 count1++;
+		if(s[i] == 'e')
+		 	count2++;
+		if(s[i] == 'i') 
+			count3++;
+		if(s[i] == 'o') 
+			count4++;
+		if(s[i] == 'u') 	
+			count5++;
 	}
-	else {
-		if (b<c){ printf ("min %d",b);}
-		else {printf("min %d",c);
-		}
-	}
-	
+	printf("Number of character:\n a: %d, e: %d, i: %d, o: %d, u: %d, rest: %d",count1,count2,count3,count4,count5,strlen(s)-count1-count2-count3-count4-count5);
+	printf("Percentages of total:\n, a: a: %.0f%%, e: %.0f%%, i: %.0f%%, o: %.0f%%, u: %.0f%%, rest: %.0f%%",(float)count1*100/strlen(s),(float)count2*100/strlen(s),(float)count3*100/strlen(s),(float)count4*100/strlen(s),(float)count5*100/strlen(s),(float)(strlen(s)-count1-count2-count3-count4-count5)*100/strlen(s));
 }
